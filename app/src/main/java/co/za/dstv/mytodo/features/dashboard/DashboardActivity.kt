2abbb4cfd94201b8353591adfc051a8f6f762fb7
Tshepo <rocboyt@gmail.com>
2020-10-06 14:17:51 +0200

    Added dashboard activity

---
+++ app/src/main/java/co/za/dstv/mytodo/features/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package co.za.dstv.mytodo.features.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.za.dstv.mytodo.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
