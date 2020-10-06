c38bc5b952d41c2b04ad2bce088c706904a1168c
Tshepo <rocboyt@gmail.com>
2020-10-06 14:11:38 +0200

    Added splash

--- app/src/main/java/co/za/dstv/mytodo/SplashActivity.kt
+++ app/src/main/java/co/za/dstv/mytodo/SplashActivity.kt
@@ -3,9 +3,8 @@ package co.za.dstv.mytodo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
